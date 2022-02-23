cbe1e78cea7440d8bc404d9c203eb6e726338d03
BPM_ccase <ccase@bridgepm.com>
2022-02-22 20:07:03 -0800

    Refresh graph after prune

--- TreeViz/treeViz.py
+++ TreeViz/treeViz.py
@@ -10,32 +10,23 @@ import pydotplus
 class TreeViz:
 
     try:
-        GLOBAL_CONFIG = json.load("config.json")
+         _GLOBAL_CONFIG = json.load("config.json")
     except AttributeError:
-        GLOBAL_CONFIG = {}
+         _GLOBAL_CONFIG = {}
 
     def __init__(self, tree_model, config={}, **kwargs):
         """
         Create a new TreeViz from an sklearn decision tree
         """
         self._tree_model = tree_model
-        self._inner_tree = tree_model.tree_
-        self._decisions = tree_model.tree_.value.argmax(axis=2).flatten().tolist()
 
         # Update config with global settings -> config arg -> applicable kwargs
-        self._config = copy.deepcopy(TreeViz.GLOBAL_CONFIG)
+        self._config = copy.deepcopy(TreeViz. _GLOBAL_CONFIG)
         self._config.update(config)
         self._config.update({k: v for k, v in kwargs.items() if k in self._config})
 
         # Generate pydotplus graph
-        dot_data = tree.export_graphviz(
-            tree_model,
-            out_file=None,
-            filled=True,
-            rounded=True,
-            special_characters=True,
-        )
-        self._graph = pydotplus.graph_from_dot_data(dot_data)
+        self._refresh_graph()
 
     def copy(self):
         """
@@ -48,27 +39,35 @@ class TreeViz:
         """
         Prune tree to remove branches where the class is the same in all children.
 
-        # RETURN
+        ### RETURN
         A new TreeViz without the pruned branches
         """
         newTreeViz = self.copy()
         newTreeViz._prune_index(0)
+        newTreeViz._refresh_graph()
         return newTreeViz
 
     def _is_leaf(self, index):
         """
         Check if inner tree node of id index is a leaf
         """
-        return (self._inner_tree.children_left[index] == TREE_LEAF
-                and self._inner_tree.children_right[index] == TREE_LEAF)
+        inner_tree = self._tree_model.tree_
+        print(index)
+        print(inner_tree.children_left[index])
+        print(inner_tree.children_right[index])
+        print((inner_tree.children_left[index] == TREE_LEAF
+                and inner_tree.children_right[index] == TREE_LEAF))
+        print()
+        return (inner_tree.children_left[index] == TREE_LEAF
+                and inner_tree.children_right[index] == TREE_LEAF)
 
     def _prune_index(self, index=0):
         """
         Turns node at index to leaf if both children have the same decisions. Recursively calls
         on children if not
         """
-        inner_tree = self._inner_tree
-        decisions = self._decisions
+        inner_tree = self._tree_model.tree_
+        decisions = inner_tree.value.argmax(axis=2).flatten().tolist()
 
         if not self._is_leaf(inner_tree.children_left[index]):
             self._prune_index(inner_tree.children_left[index])
@@ -82,8 +81,33 @@ class TreeViz:
                 and (decisions[index] == decisions[inner_tree.children_right[index]])):
 
             # turn node into a leaf by "unlinking" its children
+            print("Pruning {}".format(index))
+            print("Decision = {}".format(decisions[index]))
+            print("Before: {},{}".format(inner_tree.children_left[index], inner_tree.children_right[index]))
             inner_tree.children_left[index] = TREE_LEAF
             inner_tree.children_right[index] = TREE_LEAF
+            print("After: {},{}".format(inner_tree.children_left[index], inner_tree.children_right[index]))
+
+    def _refresh_graph(self):
+        """
+        Recalculate the graph after making changes to the tree model
+        """
+
+        dot_data = tree.export_graphviz(
+            self._tree_model,
+            out_file=None,
+            filled=True,
+            rounded=True,
+            special_characters=True,
+        )
+        self._graph = pydotplus.graph_from_dot_data(dot_data)
+
+    def write_png(self, filename):
+        """
+        Draw TreeViz as a png at filename location
+        """
+
+        self._graph.write_png(filename)
 
     def __eq__(self, other):
         """
